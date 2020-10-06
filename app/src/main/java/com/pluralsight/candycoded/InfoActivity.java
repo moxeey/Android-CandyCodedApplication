f6a2b3d8c5a12fc89a9d42f464f0c11dc4080b1d
mz_ahmad <muhdzubairuahmad@gmail.com>
2020-10-06 10:22:02 +0100

    done - startPhoneActivity test

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -37,4 +37,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View v){
+        Uri uri = Uri.parse("tel:0123456789");
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+        dialIntent.setData(uri);
+        startActivity(dialIntent);
+    }
 }
